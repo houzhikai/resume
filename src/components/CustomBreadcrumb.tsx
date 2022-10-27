bcad5eb534b86d8050660577f32e3f6c10769b59
侯志凯 <hou17513213314@163.com>
2022-10-27 23:58:57 +0800

    完成对 完成的组件 的改造

--- src/components/CustomBreadcrumb.tsx
+++ src/components/CustomBreadcrumb.tsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import { Breadcrumb } from 'antd';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 
-const CustomBreadcrumb = (props: any) => {
+const CustomBreadcrumb = (props: { arr: string[], href: string }) => {
+  const location = useLocation()
   const { arr, href } = props
-
   return (
     <Breadcrumb style={{ marginBottom: 16 }}>
       <Breadcrumb.Item>
         首页
       </Breadcrumb.Item>
       {
-        arr.map((res: any) => {
-          if (typeof res === 'object') {
-            return (
-              <Breadcrumb.Item key={res.path} >
-                <Link to={href}>{res.title}</Link>
-              </Breadcrumb.Item>
-            );
-          } else {
-            return <Breadcrumb.Item key={res}>
-              <Link to={href}>{res}</Link>
-            </Breadcrumb.Item>;
-          }
+        arr.map((res: string) => {
+          // if (typeof res === 'object') {
+          //   return (
+          //     <Breadcrumb.Item key={res.path} >
+          //       <Link to={href ?? location.pathname}>{res.title}</Link>
+          //     </Breadcrumb.Item>
+          //   );
+          // } else {
+          return <Breadcrumb.Item key={res}>
+            <Link to={href ?? location.pathname}>{res}</Link>
+          </Breadcrumb.Item>;
+          // }
         })
       }
     </Breadcrumb>
