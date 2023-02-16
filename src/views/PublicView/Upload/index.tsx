cfe4e04831be224895d561942882d9895cc5edd1
unknown <User@partner.3new.com>
2023-02-16 09:02:59 +0800

    实现替换文件内容功能

--- src/views/PublicView/Upload/index.tsx
+++ src/views/PublicView/Upload/index.tsx
@@ -1,17 +1,18 @@
-import React, { useState } from 'react';
-import CustomBreadcrumb from '../../../components/CustomBreadcrumb';
-import UseIntroduce from '../../../components/UseIntroduce';
-import { KeyTag, FormWrapper, DescribeP } from '../../../components/Div';
-import { Button, Form, Upload, message } from 'antd';
-import { UploadOutlined } from '@ant-design/icons';
+import React, { useState } from "react";
+import CustomBreadcrumb from "../../../components/CustomBreadcrumb";
+import UseIntroduce from "../../../components/UseIntroduce";
+import { KeyTag, FormWrapper, DescribeP } from "../../../components/Div";
+import { Button, Form, Upload, message, Divider } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
+import TestUpload from "./TestUpload";
 
 const UploadView = () => {
   const [fileList] = useState([]);
 
   const props = {
     beforeUpload: (file: any) => {
-      console.log('file', file);
-      const isPNG = file.type === 'image/jpeg';
+      console.log("file", file);
+      const isPNG = file.type === "image/jpeg";
       if (!isPNG) {
         message.error(`${file.name} is not a png file`);
       }
@@ -21,24 +22,42 @@ const UploadView = () => {
       console.log(111, info.fileList);
     },
   };
-  const describe = <div>
-    <DescribeP>1）暂无内容，将要写一个上传组件，实现上传图片，并且可以拿到一个其地址，达到一个<KeyTag>修改头像</KeyTag>的效果。</DescribeP>
-  </div>;
+  const describe = (
+    <div>
+      <DescribeP>
+        1）暂无内容，将要写一个上传组件，实现上传图片，并且可以拿到一个其地址，达到一个
+        <KeyTag>修改头像</KeyTag>的效果。
+      </DescribeP>
+    </div>
+  );
   return (
     <>
-      <CustomBreadcrumb arr={['通用', '上传']} />
+      <CustomBreadcrumb arr={["通用", "上传"]} />
       <UseIntroduce describe={describe} />
-      <a href='https://blog.csdn.net/Alanrnzearn/article/details/112268892' target='_blank' rel="noreferrer">推荐文章</a>
+      <a
+        href="https://blog.csdn.net/Alanrnzearn/article/details/112268892"
+        target="_blank"
+        rel="noreferrer"
+      >
+        推荐文章
+      </a>
       <FormWrapper>
         <Form>
           <Form.Item label="上传组件" required>
             <Upload {...props} defaultFileList={fileList}>
-              <Button type='primary' icon={<UploadOutlined />}>上传图片</Button>
-              <div style={{ marginTop: '10px', color: 'red' }}>支持格式：所有格式 </div>
+              <Button type="primary" icon={<UploadOutlined />}>
+                上传图片
+              </Button>
+              <div style={{ marginTop: "10px", color: "red" }}>
+                支持格式：所有格式
+              </div>
             </Upload>
           </Form.Item>
         </Form>
       </FormWrapper>
+
+      <Divider plain>测试upload读存文件</Divider>
+      <TestUpload />
     </>
   );
 };
